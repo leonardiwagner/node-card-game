fd2688331c353befbada0a095abef5edd7a6ccbc
Wagner Leonardi <leonardiwagner@gmail.com>
2014-10-30 18:17:16 -0200

    rooms concept

--- index.js
+++ index.js
@@ -3,28 +3,35 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-var routes = require('./app/routes/index.js')(app);
+//var routes = require('./app/routes/index.js')(app);
 
 app.use(express.static('public'));
 app.use(express.static('files'));
 app.use('/public', express.static('public'));
 
-mongoose.connect('mongodb://localhost/node-card-game');
+
 
 app.set('views', './app/views');
 app.use(express.static('./app/public'));
 
 
 
+
 //express's error handler middleware
-app.use(express.errorHandler());
-app.use(app.router);
+//app.use(express.errorHandler());
+//app.use(app.router);
+
+/*
+
+mongoose.connect('mongodb://localhost/node-card-game');
 
 var db = mongoose.connection;
 
 db.once('open', function(){
   console.log("hurray, connected to mongodb!");
 });
+
+*/
 
 var userSchema = mongoose.Schema({
   email: String,
@@ -37,7 +44,7 @@ var user = new User({ email: 'teste@teste.com', password: '123'});
 console.log(user.email);
 
 app.get('/', function(req, res){
-  res.sendfile('register.html');
+  res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
